b0d42a29365b3983911bebca2c1de40c5831cd5e
Matt Megarry <mattmegarry@gmail.com>
2018-02-28 13:22:45 +0100

    errors 404 & 500 returning json, BE routes returning json

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
+
+const express = require('express');
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res, next) => {
+  res.json({ yeah: 'boy' });
 });
 
 module.exports = router;
