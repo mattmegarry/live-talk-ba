06ad1f8c82ca0ddf66c375e548539ad68aef669d
Matt Megarry <mattmegarry@gmail.com>
2018-02-28 13:14:19 +0100

    mongoose and cors setup

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
+'use strict';
+
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
+// const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const index = require('./routes/index');
 const users = require('./routes/users');
 
 const app = express();
 
+// Connect to database
+mongoose.connect('mongodb://localhost/restaurant', {
+  keepAlive: true,
+  reconnectTries: Number.MAX_VALUE
+});
+
+// Middlewares
+app.use(cors());
+app.use(logger('dev'));
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
